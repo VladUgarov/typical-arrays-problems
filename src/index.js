b148aa6128e16bcbfcfa885ef1376d5e342620e6
UgarovVlad <vlad.ygarov@gmail.com>
2021-02-07 13:05:26 +0300

    function logic implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined || array.length == 0){
+        return 0;
+    }
+    else {
+        let min = array[0]
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i]
+            }
+        }
+        return min;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array.length == 0){
+        return 0;
+    }
+    let max = array[0]
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max){
+            max = array[i]
+        }
+    }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || array.length == 0){
+        return 0;
+    }
+    let sum = array[0];
+    for (let i = 1; i < array.length; i++) {
+        sum = (sum+array[i])
+        }
+
+    let avg = sum/array.length
+  return avg;
 }
